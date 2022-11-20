b790f692357692d7ed299c12a1d6a0b86dd5382a
Muhammad ryhan <mhmdryhan02@gmail.com>
2022-11-21 07:18:29 +0800

    Initial commit

---
+++ app/src/main/java/com/f55121008/githubusersf55121008/User.java
@@ -0,0 +1,33 @@
+package com.f55121008.githubusersf55121008;
+
+public class User {
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    private String photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    private String type;
+}
